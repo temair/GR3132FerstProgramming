86082c82da80a808586e2ba28a708fce93708812
Artem <artemka.irk@mail.ru>
2022-09-09 23:03:24 +0800

    add solusion for task 9,10,11,12,13,14,15

---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,59 @@
+﻿// #############################################################################################
+//                          Задача 12
+// Напишите программу, которая будет принимать на входе два числа и выводить, является ли второе число
+// кратным первому, если второе число кратное первому, то программа выводит остаток от деления.
+// #############################################################################################
+
+// Вариант Евгения
+// void VariantEvgeniy()
+// {
+//     Console.Write("Введите первое число: ");
+//     string? inputLineA = console.ReadLine();
+//     Console.Write("Введите второе число: ");
+//     string? inputLineB = console.ReadLine();
+
+//     int inputLineA = int.Parse(inputLineA);
+//     int inputLineB = int.Parse(inputLineB);
+
+//     Console.WriteLine((inputNumberB % inputNumberA == 0) ? ("Второе число кратно первому") : ("Остаток от деления: " + inputNumberB % inputNumberA));
+// }
+
+int inputNumberA = 0;
+int inputNumberB = 0;
+bool result = false;
+
+// Получаем два числа от пользователя
+void ReadDate()
+{
+    Console.Write("Введите первое число: ");
+    string? inputLineA = Console.ReadLine();
+    Console.Write("Введите второе число: ");
+    string? inputLineB = Console.ReadLine();
+
+    inputNumberA = int.Parse(inputLineA);
+    inputNumberB = int.Parse(inputLineB);
+}
+
+// Определяем кратность числа
+void ConculaData()
+{
+    result = (inputNumberB % inputNumberA == 0);
+}
+
+// Выводим данные вычисления
+void PrintData()
+{
+    if (result)
+    {
+        Console.WriteLine("Второе число кратно первому");
+    }
+    else
+    {
+        Console.WriteLine("Остаток от деления: " + inputNumberB % inputNumberA);
+    }
+}
+
+ReadDate();
+ConculaData();
+PrintData();
+
---
+++ Sem2Tesk9/Program.cs
@@ -0,0 +1,51 @@
+﻿// #############################################################################################
+//                          Задача 9
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] и
+// показывает наибольшую цифру числа.
+// #############################################################################################
+
+void MyVariant()
+{
+
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(10, 100);
+    Console.WriteLine(number);
+    int ferstDigit = number / 10;
+    int secondDigit = number % 10;
+    // Вариант 
+    if (ferstDigit > secondDigit)
+    {
+        Console.WriteLine(ferstDigit);
+    }
+    else
+    {
+        Console.WriteLine(secondDigit);
+    }
+}
+
+void VariantEvgeniy()
+{
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(10, 100);
+    Console.WriteLine(number);
+    int ferstDigit = number / 10;
+    int secondDigit = number % 10;
+    // Вариант Евгения
+    Console.WriteLine((ferstDigit > secondDigit) ? ferstDigit : secondDigit);
+}
+
+void VariatChar()
+{
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(10, 100);
+    Console.WriteLine(number);
+    int ferstDigit = number / 10;
+    int secondDigit = number % 10;
+    // Вариант char
+    char[] charArray = number.ToString().ToCharArray();
+    Console.WriteLine((int)charArray[0] > (int)charArray[1] ? charArray[0] : charArray[1]);
+}
+
+MyVariant();
+VariantEvgeniy();
+VariatChar();
